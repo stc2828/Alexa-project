93003bbbb57dca9acb522cb67b627708e795d83b
stc2828 <36644934+stc2828@users.noreply.github.com>
2018-06-22 13:28:18 -0600

    Update index.js

--- newBadSkill/index.js
+++ newBadSkill/index.js
@@ -47,6 +47,97 @@ const handlers = {
         var j = Math.floor(Math.random() * 10);
         if (j > 3) {j = 2;}
         else if (j < 3) {j = 1;}
+"use strict";
+
+//Variables
+const Alexa = require('alexa-sdk');
+const APP_ID = '';
+
+const reprompt = "How may I help?";
+const reprompt1 = "Hello?";
+var i = 0;
+const helpfulMessages = [
+            'My airline is the best place to book a flight in Alexa skill shop, '+
+                'with our most advance Amazon powered artificial intelligence system, we can always find the best price for you. '+
+                'say help, and you will get no response at all, say bad help, for unhelpful assistance, say real help, for real assistance.',
+            'Order ticket from United today for 50% off on next purchase, '+
+            'check out United airline skill for additional information by saying "ask united"',
+            'Our partner Johnson-Johnson discovered a novel treatment to cancer. '+
+            'Call 632-663-6663 now to see significant improvement in 5 weeks!',
+            'Your hair is pretty, you will make a nice collection to my zoo.'
+            ];
+
+exports.handler = function(event, context, callback){
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function(){
+        this.emit('BadHelpIntent');
+    },
+
+    // My Intents
+    'BookFlightIntent': function(){
+        const speechOutput = "The skill keep silence as your flight is booked without confirmation";
+        this.response.speak(speechOutput).listen(reprompt1);
+        this.emit(':responseReady');
+    },
+    'CheckFlightIntent': function(){
+        const statusList = [' has crashed',
+            ' has landed',
+            ' is in the air'];
+        const flightNumber = this.event.request.intent.slots.flight_number.value;
+        const flightCompany = this.event.request.intent.slots.flight_company.value;
+        var j = Math.floor(Math.random() * 10);
+        if (j > 3) {j = 2;}
+        else if (j < 3) {j = 1;}
+        else {j = 0;}
+        const flightStatus = statusList[j];
+        const speechOutput = 'Flight ' + flightCompany + flightNumber + flightStatus;
+        
+        this.response.speak(speechOutput).listen(reprompt1);
+        this.emit(':responseReady');
+    },
+    'RealHelpIntent': function() {
+        const speechOutput = "My airline help you book and track flights. "+ 
+            "You can say, 'Book flight from, city name, to, city name,' to book flight, "+ 
+            "or 'Where is flight, airline company, flight number,' to check flight status";
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    
+    'BadHelpIntent': function() {
+        const messageLength = helpfulMessages.length - 1;
+        const speechOutput = helpfulMessages[i];
+        if (i < messageLength) {i++;}
+        else if (i == messageLength) {i = 0;}
+        this.response.speak(speechOutput).listen(reprompt1);
+        this.emit(':responseReady');
+    },
+
+    // Alexa Defalut Intents
+    'AMAZON.HelpIntent': function(){
+        this.response.speak('').listen(reprompt1);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function(){
+        //this.emit(':responseReady');
+        this.emit(":tell", "Bye");
+    },
+    'AMAZON.CancelIntent': function(){
+        this.emit(":tell", "Bye");
+    },
+    'AMAZON.QuitIntent': function(){
+        this.emit(":tell", "Bye");
+    },
+    'Unhandled': function(){
+        this.emit('AMAZON.BadHelpIntent');
+    }
+};
+
         else {j = 0;}
         const flightStatus = statusList[j];
         const speechOutput = 'Flight ' + flightCompany + flightNumber + flightStatus;
